c96d297ebd339993016cf483b9ce40c13d524609
agoujjimyassine <agoujjimyassine@gmail.com>
2021-02-24 16:51:00 +0100

    second change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfilComponent } from './profil/profil.component';
+import { AdministratifComponent } from './administratif/administratif.component';
+import { AdresseComponent } from './adresse/adresse.component';
+import { ResponsablesComponent } from './responsables/responsables.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfilComponent,
+    AdministratifComponent,
+    AdresseComponent,
+    ResponsablesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
